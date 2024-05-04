e127360ecfa235af40ad365e38b0c1ae8205dffb
Zoe Roux <zoe.roux@zoriya.dev>
2024-05-04 13:24:08 +0200

    Create AddFeed dto, add validation and sqlx

--- api/cmd/main.go
+++ api/cmd/main.go
@@ -1,23 +1,38 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
+	"github.com/go-playground/validator/v10"
+	"github.com/jmoiron/sqlx"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	_ "github.com/lib/pq"
+	"github.com/zoriya/vex"
 )
 
 type Handler struct {
-	database *sql.DB
+	feeds vex.FeedService
 }
 
 func (h *Handler) GetEntries(c echo.Context) error {
 	ret := make([]interface{}, 0)
 	return c.JSON(200, ret)
+}
+
+type Validator struct {
+	validator *validator.Validate
+}
+
+func (cv *Validator) Validate(i interface{}) error {
+	if err := cv.validator.Struct(i); err != nil {
+		// Optionally, you could return the error to give each route more control over the status code
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+	return nil
 }
 
 func main() {
@@ -29,15 +44,16 @@ func main() {
 		os.Getenv("POSTGRES_PORT"),
 		os.Getenv("POSTGRES_DB"),
 	)
-	db, err := sql.Open("postgres", con)
+	db, err := sqlx.Open("postgres", con)
 	if err != nil {
 		log.Fatal(err)
 	}
 	h := Handler{
-		database: db,
+		feeds: vex.NewFeedService(db),
 	}
 
 	e := echo.New()
+	e.Validator = &Validator{validator: validator.New()}
 	e.Use(middleware.Logger())
 	e.GET("/entries", h.GetEntries)
 	h.RegisterFeedsRoutes(e)
--- api/go.mod
+++ api/go.mod
@@ -3,14 +3,20 @@ module github.com/zoriya/vex
 go 1.22.1
 
 require (
+	github.com/go-playground/validator/v10 v10.20.0
 	github.com/google/uuid v1.6.0
+	github.com/jmoiron/sqlx v1.4.0
 	github.com/labstack/echo/v4 v4.12.0
 	github.com/lib/pq v1.10.9
 )
 
 require (
+	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
+	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
